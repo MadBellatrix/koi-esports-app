79a95191a8fad9abd8918e3a082d13ae8c181a3a
MadBellatrix <loyalslytherindeatheater@gmail.com>
2026-09-09 21:07:42 +0200

    Behebe den Vite Base-Pfad für das Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // WICHTIG: Das sorgt dafür, dass alle Asset-Pfade relativ generiert werden!
 })
